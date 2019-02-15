7acb1f7fddc80df7be12821aaf9fa8f90f70df0d
ekampourakis <34644857+ekampourakis@users.noreply.github.com>
2019-02-15 15:40:33 +0200

    Basic serial functionality and design

--- UoP Telemetry GUI/Main.Designer.vb
+++ UoP Telemetry GUI/Main.Designer.vb
@@ -22,17 +22,153 @@ Partial Class Main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox_Connection = New System.Windows.Forms.GroupBox()
+        Me.Label_Serial = New System.Windows.Forms.Label()
+        Me.ComboBox_Ports = New System.Windows.Forms.ComboBox()
+        Me.Button_Connect = New System.Windows.Forms.Button()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel_Status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel_SerialInfo = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel_Spring = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel_Updates = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer_Serial = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer_SendQueue = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer_Status = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer_GUIUpdate = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox_Connection.SuspendLayout()
+        Me.StatusStrip.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'GroupBox_Connection
+        '
+        Me.GroupBox_Connection.Controls.Add(Me.Label_Serial)
+        Me.GroupBox_Connection.Controls.Add(Me.ComboBox_Ports)
+        Me.GroupBox_Connection.Controls.Add(Me.Button_Connect)
+        Me.GroupBox_Connection.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox_Connection.Name = "GroupBox_Connection"
+        Me.GroupBox_Connection.Size = New System.Drawing.Size(290, 65)
+        Me.GroupBox_Connection.TabIndex = 7
+        Me.GroupBox_Connection.TabStop = False
+        Me.GroupBox_Connection.Text = "Connection"
+        '
+        'Label_Serial
+        '
+        Me.Label_Serial.AutoSize = True
+        Me.Label_Serial.Location = New System.Drawing.Point(6, 29)
+        Me.Label_Serial.Name = "Label_Serial"
+        Me.Label_Serial.Size = New System.Drawing.Size(36, 13)
+        Me.Label_Serial.TabIndex = 3
+        Me.Label_Serial.Text = "Serial:"
+        '
+        'ComboBox_Ports
+        '
+        Me.ComboBox_Ports.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox_Ports.FormattingEnabled = True
+        Me.ComboBox_Ports.Location = New System.Drawing.Point(48, 25)
+        Me.ComboBox_Ports.Name = "ComboBox_Ports"
+        Me.ComboBox_Ports.Size = New System.Drawing.Size(155, 21)
+        Me.ComboBox_Ports.TabIndex = 4
+        '
+        'Button_Connect
+        '
+        Me.Button_Connect.Location = New System.Drawing.Point(209, 24)
+        Me.Button_Connect.Name = "Button_Connect"
+        Me.Button_Connect.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Connect.TabIndex = 1
+        Me.Button_Connect.Text = "Connect"
+        Me.Button_Connect.UseVisualStyleBackColor = True
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel_Status, Me.ToolStripStatusLabel_SerialInfo, Me.ToolStripStatusLabel_Spring, Me.ToolStripStatusLabel_Updates})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 439)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(845, 22)
+        Me.StatusStrip.TabIndex = 8
+        Me.StatusStrip.Text = "StatusStrip"
+        '
+        'ToolStripStatusLabel_Status
+        '
+        Me.ToolStripStatusLabel_Status.ForeColor = System.Drawing.Color.Firebrick
+        Me.ToolStripStatusLabel_Status.Name = "ToolStripStatusLabel_Status"
+        Me.ToolStripStatusLabel_Status.Size = New System.Drawing.Size(79, 17)
+        Me.ToolStripStatusLabel_Status.Text = "Disconnected"
+        '
+        'ToolStripStatusLabel_SerialInfo
+        '
+        Me.ToolStripStatusLabel_SerialInfo.Name = "ToolStripStatusLabel_SerialInfo"
+        Me.ToolStripStatusLabel_SerialInfo.Size = New System.Drawing.Size(101, 17)
+        Me.ToolStripStatusLabel_SerialInfo.Text = "0 Packets Queued"
+        '
+        'ToolStripStatusLabel_Spring
+        '
+        Me.ToolStripStatusLabel_Spring.Name = "ToolStripStatusLabel_Spring"
+        Me.ToolStripStatusLabel_Spring.Size = New System.Drawing.Size(512, 17)
+        Me.ToolStripStatusLabel_Spring.Spring = True
+        '
+        'ToolStripStatusLabel_Updates
+        '
+        Me.ToolStripStatusLabel_Updates.Name = "ToolStripStatusLabel_Updates"
+        Me.ToolStripStatusLabel_Updates.Size = New System.Drawing.Size(138, 17)
+        Me.ToolStripStatusLabel_Updates.Text = "Made by - ekampourakis"
+        '
+        'SerialPort
+        '
+        Me.SerialPort.BaudRate = 115200
+        Me.SerialPort.PortName = "COM5"
+        '
+        'Timer_Serial
+        '
+        Me.Timer_Serial.Enabled = True
+        Me.Timer_Serial.Interval = 10
+        '
+        'Timer_SendQueue
+        '
+        Me.Timer_SendQueue.Enabled = True
+        Me.Timer_SendQueue.Interval = 10
+        '
+        'Timer_Status
+        '
+        Me.Timer_Status.Enabled = True
+        Me.Timer_Status.Interval = 1500
+        '
+        'Timer_GUIUpdate
+        '
+        Me.Timer_GUIUpdate.Enabled = True
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(666, 439)
+        Me.ClientSize = New System.Drawing.Size(845, 461)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.Controls.Add(Me.GroupBox_Connection)
         Me.Name = "Main"
-        Me.Text = "Main"
+        Me.ShowIcon = False
+        Me.Text = "UoP Telemetry GUI"
+        Me.GroupBox_Connection.ResumeLayout(False)
+        Me.GroupBox_Connection.PerformLayout()
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents GroupBox_Connection As GroupBox
+    Friend WithEvents Label_Serial As Label
+    Friend WithEvents ComboBox_Ports As ComboBox
+    Friend WithEvents Button_Connect As Button
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents ToolStripStatusLabel_Status As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel_SerialInfo As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel_Updates As ToolStripStatusLabel
+    Friend WithEvents SerialPort As IO.Ports.SerialPort
+    Friend WithEvents Timer_Serial As Timer
+    Friend WithEvents Timer_SendQueue As Timer
+    Friend WithEvents Timer_Status As Timer
+    Friend WithEvents Timer_GUIUpdate As Timer
+    Friend WithEvents ToolStripStatusLabel_Spring As ToolStripStatusLabel
 End Class
--- UoP Telemetry GUI/Main.vb
+++ UoP Telemetry GUI/Main.vb
@@ -1,3 +1,318 @@
 ﻿Public Class Main
 
+#Region "Callbacks"
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+    Private Sub SetText(ByVal [text] As String)
+        If Me.InvokeRequired() Then
+            Dim d As New SetTextCallback(AddressOf SetText)
+            Me.Invoke(d, New Object() {[text]})
+        Else
+            Me.ToolStripStatusLabel_Updates.Text = [text]
+        End If
+    End Sub
+    Delegate Sub SetColorCallback(ByVal [color] As Color)
+    Private Sub SetColor(ByVal [color] As Color)
+        If InvokeRequired() Then
+            Dim d As New SetColorCallback(AddressOf SetColor)
+            Invoke(d, New Object() {[color]})
+        Else
+            ToolStripStatusLabel_Updates.BackColor = [color]
+        End If
+    End Sub
+    Delegate Sub SetTimerCallback(ByVal [enable] As Boolean)
+    Private Sub SetTimer(ByVal [enable] As Boolean)
+        If InvokeRequired() Then
+            Dim d As New SetTimerCallback(AddressOf SetTimer)
+            Invoke(d, New Object() {[enable]})
+        Else
+            Timer_Status.Enabled = [enable]
+        End If
+    End Sub
+#End Region
+
+#Region "ComboBox Live Update"
+    Private Sub ComboBox_Ports_Click(sender As Object, e As EventArgs) Handles ComboBox_Ports.Click
+        Try
+            ComboBox_Ports.Items.Clear()
+            For Each sp As String In My.Computer.Ports.SerialPortNames
+                ComboBox_Ports.Items.Add(sp)
+            Next
+            ComboBox_Ports.SelectedIndex = 0
+        Catch err As Exception
+            ComboBox_Ports.Items.Clear()
+        End Try
+    End Sub
+#End Region
+
+#Region "Serial Variables"
+    Private Connected As Boolean = False
+    Private IsWaiting As Boolean = False
+    Private DataReady As Boolean = False
+    Private WaitingID As Byte = 0
+    Private LastReceived As DateTime = DateTime.Now
+    Private RX As New List(Of Byte)
+    Private RXData As Byte()
+    Private Const MaxRX As Integer = 255
+    Private Const ReceiveTimeout As Integer = 100
+    Private TXQueue As New List(Of Byte())
+    Private PacketsProcessed As Integer = 0
+#End Region
+
+#Region "Receive"
+    Private Sub SerialPort_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
+        While SerialPort.BytesToRead > 0
+            If IsWaiting Then
+                RX.Add(SerialPort.ReadByte())
+                LastReceived = DateTime.Now
+                ProcessRX()
+            Else
+                ' Read trash
+                SerialPort.ReadByte()
+            End If
+        End While
+    End Sub
+
+    Private Sub ProcessRX()
+        If RX.Count > 0 Then
+            Dim len As Integer = 0
+            If RX(0) = &H2 Then
+                If RX.Count >= 2 Then
+                    len = RX(1)
+                    If RX.Count = 3 Then
+                        If RX(2) = WaitingID Then
+                        Else
+                            ' Command ID not correct
+                            ReceiveFailed()
+                            Exit Sub
+                        End If
+                    End If
+                    If RX.Count >= 5 + len Then
+                        If RX(4 + len) = &H3 Then
+                            Dim Data(len - 1) As Byte
+                            For Index As Integer = 2 To len + 1
+                                Data(Index - 2) = RX(Index)
+                            Next
+                            Dim CheckSum As CRC.CheckSum = CRC.CRC(Data)
+                            If (CheckSum.First = RX(2 + len) And CheckSum.Second = RX(3 + len)) Then
+                                ' Cheksum success
+                                RXData = Data
+                                ReceiveSuccess()
+                            Else
+                                ' Failed checksum
+                                ReceiveFailed()
+                                Exit Sub
+                            End If
+                        Else
+                            ' Last byte not correct
+                            ReceiveFailed()
+                            Exit Sub
+                        End If
+                    End If
+                End If
+            Else
+                ' First byte not correct
+                ReceiveFailed()
+                Exit Sub
+            End If
+        End If
+    End Sub
+
+    Private Sub ReceiveFailed()
+        DisplayStatus("Receive failed", Color.Firebrick)
+        IsWaiting = False
+        RX.Clear()
+    End Sub
+
+    Private Sub ReceiveSuccess()
+        IsWaiting = False
+        DataReady = True
+        RX.RemoveRange(0, RXData.Length + 5)
+        DisplayStatus("Success", Color.Green)
+        If Connected = False Then
+            Connected = True
+        End If
+    End Sub
+
+    Private Sub Timer_Serial_Tick(sender As Object, e As EventArgs) Handles Timer_Serial.Tick
+        If IsWaiting Then
+            Dim Span As TimeSpan = DateTime.Now - LastReceived
+            If Span.TotalMilliseconds > ReceiveTimeout Then
+                ReceiveFailed()
+            End If
+        End If
+        If DataReady Then
+            ProcessData()
+        End If
+    End Sub
+#End Region
+
+#Region "Send"
+    Private Sub Send(ByVal Bytes As Byte())
+        TXQueue.Add(Bytes)
+    End Sub
+
+    Private Sub Timer_SendQueue_Tick(sender As Object, e As EventArgs) Handles Timer_SendQueue.Tick
+        If Not IsWaiting And TXQueue.Count > 0 And SerialPort.IsOpen Then
+            Dim Bytes As Byte() = TXQueue(0)
+            Dim PrefixBytes As Byte() = {&H2, Bytes.Length}
+            Dim Check As CRC.CheckSum = CRC.CRC(Bytes)
+            Dim SuffixBytes As Byte() = {Check.First, Check.Second, &H3}
+            SerialPort.Write(PrefixBytes, 0, PrefixBytes.Length)
+            SerialPort.Write(Bytes, 0, Bytes.Length)
+            SerialPort.Write(SuffixBytes, 0, SuffixBytes.Length)
+            LastReceived = DateTime.Now
+            IsWaiting = True
+            WaitingID = Bytes(0)
+            TXQueue.RemoveAt(0)
+        End If
+    End Sub
+
+    Private Sub ToolStripStatusLabel_SerialInfo_Click(sender As Object, e As EventArgs) Handles ToolStripStatusLabel_SerialInfo.Click
+        TXQueue.Clear()
+    End Sub
+#End Region
+
+#Region "Process"
+    Private Sub ProcessData()
+        'Try
+        '    If RXData(0) = COMM_PACKET_ID.COMM_CONNECTION_PACKET Then
+        '        ProcessSuccess()
+        '    ElseIf RXData(0) = COMM_PACKET_ID.COMM_GET_INFO Then
+        '        LoadInfo()
+        '        ProcessSuccess()
+        '    ElseIf RXData(0) = COMM_PACKET_ID.COMM_GET_VOLTAGES Then
+        '        LoadVoltages()
+        '        ProcessSuccess()
+        '    ElseIf RXData(0) = COMM_PACKET_ID.COMM_GET_TEMPERATURES Then
+        '        LoadTemperatures()
+        '        ProcessSuccess()
+        '    ElseIf RXData(0) = COMM_PACKET_ID.COMM_GET_CURRENTS Then
+        '        LoadCurrents()
+        '        ProcessSuccess()
+        '    ElseIf RXData(0) = COMM_PACKET_ID.COMM_CAN_FORWARD Then
+        '        LoadCAN()
+        '        ProcessSuccess()
+        '    End If
+        'Catch ex As Exception
+        '    ProcessSuccess()
+        '    'MsgBox("Data process error. " & ex.Message, MsgBoxStyle.Critical, "Error")
+        '    Console.WriteLine(ex.Message)
+        'End Try
+    End Sub
+
+    Private Sub ProcessSuccess()
+        DataReady = False
+        RXData = {}
+        PacketsProcessed += 1
+    End Sub
+#End Region
+
+#Region "GUI"
+    Private Sub Timer_GUIUpdate_Tick(sender As Object, e As EventArgs) Handles Timer_GUIUpdate.Tick
+        If Connected Then
+            ToolStripStatusLabel_Status.ForeColor = Color.Green
+            ToolStripStatusLabel_Status.Text = "Connected"
+            Button_Connect.Text = "Disconnect"
+        Else
+            ToolStripStatusLabel_Status.ForeColor = Color.Firebrick
+            ToolStripStatusLabel_Status.Text = "Disconnected"
+            Button_Connect.Text = "Connect"
+        End If
+        ToolStripStatusLabel_SerialInfo.Text = "Queued: " & TXQueue.Count & " | Processed: " & PacketsProcessed
+    End Sub
+
+    Private Sub DisplayStatus(ByVal Message As String, ByVal Color As Color)
+        SetText(Message)
+        SetColor(Color)
+        SetTimer(True)
+    End Sub
+
+    Private Sub Timer_Status_Tick(sender As Object, e As EventArgs) Handles Timer_Status.Tick
+        ToolStripStatusLabel_Updates.Text = ""
+        SetTimer(False)
+    End Sub
+#End Region
+
+#Region "Buttons"
+    Private Sub Button_Connect_Click(sender As Object, e As EventArgs) Handles Button_Connect.Click
+        'Try
+        If Button_Connect.Text = "Connect" Then
+            If SerialPort.IsOpen Then
+                SerialPort.Close()
+            End If
+            SerialPort.PortName = ComboBox_Ports.SelectedItem
+            SerialPort.Open()
+            System.Threading.Thread.Sleep(110)
+            Send({0})
+        Else
+            SerialPort.Close()
+            Connected = False
+        End If
+
+    End Sub
+#End Region
+
+#Region "Arithmetic"
+    Private Function GetUInt16(ByRef Bytes As Byte(), ByRef StartIndex As Integer, ByVal Optional BigEndian As Boolean = True) As UInt16
+        Dim Result As UInt16
+        If BigEndian Then
+            Result = BitConverter.ToUInt16({Bytes(StartIndex + 1), Bytes(StartIndex)}, 0)
+            StartIndex += 2
+        Else
+            Result = BitConverter.ToUInt16(Bytes, StartIndex)
+            StartIndex += 2
+        End If
+        Return Result
+    End Function
+
+    Private Function GetInt32(ByRef Bytes As Byte(), ByRef StartIndex As Integer, ByVal Optional BigEndian As Boolean = True) As Int32
+        Dim Result As Int32
+        If BigEndian Then
+            Result = BitConverter.ToInt32({Bytes(StartIndex + 3), Bytes(StartIndex + 2), Bytes(StartIndex + 1), Bytes(StartIndex)}, 0)
+            StartIndex += 4
+        Else
+            Result = BitConverter.ToInt32(Bytes, StartIndex)
+            StartIndex += 4
+        End If
+        Return Result
+    End Function
+
+    Private Function GetUInt32(ByRef Bytes As Byte(), ByRef StartIndex As Integer, ByVal Optional BigEndian As Boolean = True) As UInt32
+        Dim Result As UInt32
+        If BigEndian Then
+            Result = BitConverter.ToUInt32({Bytes(StartIndex + 3), Bytes(StartIndex + 2), Bytes(StartIndex + 1), Bytes(StartIndex)}, 0)
+            StartIndex += 4
+        Else
+            Result = BitConverter.ToUInt32(Bytes, StartIndex)
+            StartIndex += 4
+        End If
+        Return Result
+    End Function
+
+    Private Function GetByte(ByRef Bytes As Byte(), ByRef StartIndex As Integer)
+        Dim Result As Byte = Bytes(StartIndex)
+        StartIndex += 1
+        Return Result
+    End Function
+
+    Private Function ToDegrees(ByVal TemperatureVoltage As UInt16) As Single
+        Dim Voltage As Double = TemperatureVoltage * 0.0001
+        Const Rdiv As Double = 10000.0F
+        Const Rref As Double = 10000.0F
+        Const Vref As Double = 3.0F
+        Dim Rtherm As Double = (Voltage * Rdiv) / (Vref - Voltage)
+        Const A As Double = 0.003354016F
+        Const B As Double = 0.000256985F
+        Const C As Double = 0.000002620131F
+        Const D As Double = 0.00000006383091F
+        Dim LN As Double = Math.Log(Rtherm / Rref)
+        Dim T As Double = 1.0 / (A + B * LN + C * Math.Pow(LN, 2) + D * Math.Pow(LN, 3))
+        Return TemperatureVoltage - 273.15F
+    End Function
+#End Region
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
 End Class
