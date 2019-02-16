5bd42c399b1398488361049f394555a67481c2a0
ekampourakis <34644857+ekampourakis@users.noreply.github.com>
2019-02-16 13:23:36 +0200

    Created telemetry structures

--- UoP Telemetry GUI/Definitions.vb
+++ UoP Telemetry GUI/Definitions.vb
@@ -68,6 +68,63 @@
         Public Current_Low_Battery As Single
     End Structure
 
+    Public Structure Packet_Performance
+        Public RPM As Int16
+        Public Torque As Int16
+        Public IVT_Current As Int16
+        Public IVT_Voltage As UInt16
+    End Structure
+
+    Public Structure Packet_BMS
+        Public Faults As Byte
+        Public Voltage_Min_Left As Byte
+        Public Voltage_Min_Right As Byte
+        Public Voltage_Max_Left As Byte
+        Public Voltage_Max_Right As Byte
+        Public Temp_Min_Left As Byte
+        Public Temp_Max_Left As Byte
+        Public Temp_Min_Right As Byte
+        Public Temp_Max_Right As Byte
+
+    End Structure
+
+    Public Structure Packet_Temps
+        Public IGBT As Byte
+        Public Motor As Byte
+        Public Coolant_In As Byte
+        Public Coolant_Out As Byte
+        Public Gearbox As Byte
+    End Structure
+
+    Public Structure Packet_Pedals
+        Public Throttle_12 As Byte
+        Public Brake_Front As Byte
+        Public Brake_Rear As Byte
+    End Structure
+
+    Public Structure Packet_Wheels
+        Public RPM_Front_Left As Byte
+        Public RPM_Front_Right As Byte
+        Public RPM_Rear_Left As Byte
+        Public RPM_Rear_Right As Byte
+    End Structure
+
+    Public Structure Packet_VCU
+        Public ETD As Boolean
+        Public BMSA As Boolean
+        Public MCMSAs As Byte
+        Public EMA As UInt16
+        Public CDSAs As Byte
+        Public PLS As UInt16
+        Public Current_Low_Battery As UInt16
+    End Structure
+
+    Public Structure Packet_IMU
+        Public X As Single
+        Public Y As Single
+        Public Z As Single
+    End Structure
+
     Public Structure Car_Mixed
         Public Raw As Car_Raw
         Public Processed As Car_Processed
